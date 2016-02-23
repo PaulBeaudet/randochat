b93449fb1d8b3bea41c5f977518d4c591de507c4
paulBeaudet <inof8or@gmail.com>
2016-02-23 16:58:37 -0500

    Initial working commit of project

---
+++ serve.js
@@ -0,0 +1,106 @@
+// serve.js ~ Copyright 2015 Paul Beaudet ~ Licence Affero GPL ~ See LICENCE_AFFERO for details
+const RECONSIDER = 10000;  // milliseconds to rematching
+
+var when = {
+    idle: true,
+    users: [],
+    connected: function(socket){
+        var cookieCrums = socket.request.headers.cookie.split('=');   // split correct cookie out
+        var user = cookie.user(cookieCrums[cookieCrums.length - 1]);  // decrypt email from cookie, make it userID
+        if(user){user = user.content.user}                            // check for existing cookie
+        else{return 0};                                               // this is a nameless (expired) socket!
+
+        if(user.name){                                                // given a valid user connected
+            console.log(user.name + ' connected id:' + socket.id);    // log connection event
+            sock.ets.to(socket.id).emit('youAre', user.name);         // make sure the socket knows who it is
+
+            // connect this user with another or put them on the waiting list
+            when.match(socket.id);
+        }
+        return user.name;
+    },
+    match: function(socketID){
+        if(when.users.length){                                         // given there are other users waiting
+            var rando = Math.floor(Math.random() * when.users.length); // grab rando user to connect with
+            sock.ets.to(when.users[rando]).emit('start', socketID);    // get into a conversation with rando
+            sock.ets.to(socketID).emit('start', when.users[rando]);    // make sure this socket knows it
+            when.users.splice(rando, 1);                               // remove user from list
+        } else {                                                       // place on waiting list
+            when.users.push(socketID);                                 // if no users push to list
+        }
+    },
+    end: function(socketID, nick){
+        when.match(socketID);
+        // find a new match
+    },
+}
+
+var sock = {
+    ets: require('socket.io'),
+    listen: function(server){
+        sock.ets = sock.ets(server);
+        sock.ets.on('connection', function(socket){
+            if(!when.connected(socket)){return;}          // provide no services in the case of a failed connection
+            socket.on('chat', function(rtt){sock.ets.to(rtt.to).emit('chat', rtt);});   // emit rtt to partner
+            socket.on('interrupt', function(rtt){sock.ets.to(rtt.to).emit('interrupt', rtt);});
+            socket.on('end', function(){when.match(socket.id);});
+            socket.on('disconnect', function(){console.log(socket.id + ' disconnected');});
+        });
+    },
+}
+
+var userAct = { // dep: mongo
+    auth: function ( render ){
+        return function(req, res){
+            if(req.session.user && req.session.user.name){
+                console.log(req.session.user.name + ' joined')
+                res.render(render);
+            } else {res.redirect('/');}
+        }
+    },
+    name: function ( req, res ){
+        if(req.body.name){
+            req.session.user = {name: req.body.name};
+            res.redirect('/chat');
+        } else {
+            res.redirect('/');
+        }
+    }
+}
+
+var cookie = { // depends on client-sessions and mongo
+    session: require('client-sessions'),
+    ingredients: {
+        cookieName: 'session',
+        secret: process.env.SESSION_SECRET,
+        duration: 5 * 365 * 24 * 60 * 60 * 1000,  // cookie times out in 5 years
+    },
+    meWant: function(){return cookie.session(cookie.ingredients);},
+    user: function(content){return cookie.session.util.decode(cookie.ingredients, content);}, // decode cookie for socket reactions
+}
+
+var serve = {
+    express: require('express'),
+    parse: require('body-parser'),
+    theSite: function (){
+        var app = serve.express();
+        var http = require('http').Server(app);            // http server for express framework
+        app.set('view engine', 'jade');                    // template with jade
+        app.use(require('compression')());                 // gzipping for requested pages
+        app.use(serve.parse.json());                       // support JSON-encoded bodies
+        app.use(serve.parse.urlencoded({extended: true})); // support URL-encoded bodies
+        app.use(cookie.meWant());                          // support for cookies
+
+        app.use(serve.express.static(__dirname + '/views')); // serve page dependancies (sockets, jquery, bootstrap)
+        var router = serve.express.Router();
+        router.get('/', function(req, res){res.render('name');});
+        router.post('/', userAct.name);
+        router.get('/chat', userAct.auth('chat'));
+        app.use(router);
+
+        sock.listen(http);                            // listen for socket connections
+        http.listen(process.env.PORT);                // listen on specified PORT enviornment variable
+    }
+}
+
+serve.theSite(); //Initiate the site
---
+++ views/client.js
@@ -0,0 +1,134 @@
+// client.js ~ Copyright 2015 Paul Beaudet ~ MIT License see LICENSE_MIT for detials
+var NUM_ENTRIES = 6;   // number of dialog rows allowed in the application
+var OPEN_HELM = 25;    // time before helm can be taken by interuption
+var FULL_TIMEOUT = 15; // timeout for long inactivity
+var PAUSE_TIMEOUT = 3; // inactivity timeout
+
+var hist = {
+    row: 0,                                    // notes history row being typed in
+    refresh: function(){                       // refresh elements
+        for(var i = 0; i<NUM_ENTRIES; i++){    // for all of the entries
+            $('#dialog'+i).html('');           // clear previous dialog
+            $('#person'+i).html('');           // clear previous users
+        }
+        hist.row = 0;
+        $('#wpm').html('');
+    },
+    increment: function(){ // decides with row to edit to and when the dialog needs to scoot up
+        if(hist.row === NUM_ENTRIES - 1){
+            for(var i = 1; i < NUM_ENTRIES; i++){
+                $('#dialog' + ( i - 1 )).html( $('#dialog' + i).html() );
+                $('#person' + ( i - 1 )).html( $('#person' + i).html() );
+            }
+            $('#dialog' + (NUM_ENTRIES - 1)).html('');
+            $('#person' + (NUM_ENTRIES -1)).html('');
+        }
+        else {hist.row++;}
+    },
+    chat: function(rtt){
+        $('#dialog'+ hist.row).html(rtt.text);                  // incomming chat dialog
+        $('#person'+ hist.row).html(rtt.from);                  // set id of incoming message
+        myTurn.idle = 0;                                        // reset idle counter
+        $('#wpm').html(speed.realTime(rtt.text.length)+' WPM'); // show speed after start
+    },
+    taken: function(){ return $('#person'+hist.row).html() === send.to; }, // check if our partner started talking
+}
+
+var myTurn = {
+    isIt: false,
+    elapsed: 0,
+    idle: 0,
+    clock: 0,
+    interrupt: function(rtt){
+        myTurn.set(false);
+        send.clear();
+        myTurn.begin();
+        hist.chat(rtt);
+    },
+    set: function(status){
+        myTurn.isIt = status;                                 // set status of whos turn it is
+        $('#sendText').html(myTurn.isIt ? 'Type!' : 'Wait!'); // send text reflects ability wether it be true or false
+    },
+    start: function(){ // first turn
+        myTurn.elapsed = 0;
+        clearTimeout(myTurn.clock);                    // Make sure standing timeout is removed if any
+        myTurn.clock = setTimeout(myTurn.check, 1000); // create new timeout
+    },
+    begin: function(){ // turn by turn
+        hist.increment();
+        speed.realTime();
+        myTurn.start();
+    },
+    check: function(){
+        myTurn.elapsed++;                   // increment elapsed time
+        myTurn.idle++;                      // increment idle time (will only really increment w/inactivity)
+        if(myTurn.isIt){                    // might this client talk?
+            if($('#person'+hist.row).html()===send.to){myTurn.set(false);} // if someone all ready talk then no
+        }else{                              // not this users turn
+            if(myTurn.elapsed > OPEN_HELM || myTurn.idle > PAUSE_TIMEOUT){myTurn.set(true);} // check for my turn
+        }
+        if(myTurn.idle > FULL_TIMEOUT){
+            console.log('disconnecting');
+            sock.et.emit('end');
+            hist.refresh();
+            myTurn.set(false);
+            myTurn.idle = 0;
+        } else {myTurn.clock = setTimeout(myTurn.check, 1000);} // only set timeout in non-disconnect events
+    }
+}
+
+var send = {
+    empty: true, // only way to know text was clear before typing i.e. client just stared typing
+    to: '',      // note other socket being messaged with
+    input: function(){
+        if(myTurn.isIt){
+            var rtt = {text: $('#textEntry').val(), to: send.to, from: sock.nick};
+            if(send.empty){
+                send.empty = false;
+                sock.et.emit('interrupt', rtt);
+                myTurn.begin();
+            } else {
+                sock.et.emit('chat', rtt);       // send real time chat data to partner
+            }
+            hist.chat(rtt);                      // write chat real time chat data to self
+        } else {send.clear();}                   // block input
+    },
+    clear: function(){
+        $('#textEntry').val(''); // clear out text in entry bar
+        send.empty = true;       // note that text is cleared out of entry bar
+    },
+}
+
+var speed = { // -- handles gathing speed information
+    start: 0,
+    realTime: function(chars){
+        var time = new Date();
+        var now = time.getTime();
+        if(chars){return (60000/((now-speed.start)/chars)/5).toFixed(2);} // return words per minute
+        else { speed.start = now; }                                       // no param/chars starts the clock
+    },
+}
+
+var sock = {  // -- handle socket.io connection events
+    et: io(), // start socket.io listener
+    nick: '', // name of this client
+    name: function(nickName){           // allow chat and go when we have a name
+        sock.nick = nickName;           // learn ones own name
+        sock.et.on('chat', hist.chat);  // recieves real time chat information
+        sock.et.on('interrupt', myTurn.interrupt); // recieves new chat partners or interuptions from partner
+        sock.et.on('connect_error', function(){window.location.replace('/chat');}); // reload on connection error
+        sock.et.on('start', function(partner){
+            console.log('connecting with ' + partner );
+            send.to = partner;          // recognize who you're talking to
+            myTurn.set(true);           // give the ability to talk
+            myTurn.start();             // signal begining of turn
+        });
+    }
+}
+
+$(document).ready(function(){                                  // when DOM is ready
+    myTurn.set(false);                                         // Block untill server gives a match
+    sock.et.on('youAre', sock.name);                           // wait for decrypted nickname
+    $('#textEntry').keydown(send.enter);                       // capture special key like enter
+    document.getElementById('textEntry').oninput = send.input; // listen for input event
+});
