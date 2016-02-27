1bd82b900b2dbaf26bfe2f10244299c8dd39d0d8
paulBeaudet <inof8or@gmail.com>
2016-02-26 23:21:31 -0500

    add persistence for users

--- serve.js
+++ serve.js
@@ -31,11 +31,7 @@ var when = {
         } else {                                                       // place on waiting list
             when.users.push(socketID);                                 // if no users push to list
         }
-    },
-    end: function(socketID, nick){
-        when.match(socketID);
-        // find a new match
-    },
+    }
 }
 
 var sock = {
@@ -52,26 +48,66 @@ var sock = {
     },
 }
 
+var mongo = { // depends on: mongoose
+    db: require('mongoose'),
+    init: function(){
+        mongo.db.connect(process.env.MONGOLAB_URI);                               // connect to our database
+        var Schema = mongo.db.Schema; var ObjectId = Schema.ObjectId;
+        mongo.user = mongo.db.model('user', new Schema({                          // create user object property
+            id: ObjectId,                                                         // unique id of document
+            name: { type: String, required: '{PATH} is required', unique: true }, // Name of user
+            password: { type: String, required: '{PATH} is required' },           // user password
+            type: {type: String},                                                 // type of account, admin, mod, ect
+        }));
+    }
+}
+
 var userAct = { // dep: mongo
-    auth: function(req, res){
+    hash: require('bcryptjs'),                                // hash passwords with bcrypt
+    auth: function(req, res){                                 // make sure user has a name
         if(req.session.user && req.session.user.name){
             console.log(req.session.user.name + ' joined')
             res.render('chat');
-        } else {res.redirect('/signin');}
+        } else {res.redirect('/signin');} // redirect to signin route if this user has yet self name
     },
-    name: function(req, res){
-        if(req.body.name){
-            req.session.user = {name: req.body.name};
-            res.redirect('/');
-        } else {
-            res.redirect('/signin'); // maybe re-render with an error message
-        }
+    login: function(req, res){
+        if(req.body.password && req.body.name){
+            mongo.user.findOne({name: req.body.name}, function(err, rando){          // find a rando in our db
+                if(rando){                                                           // say one of our little randos exist
+                    if(userAct.hash.compareSync(req.body.password, rando.password)){ // check if their password is right
+                        userAct.chat(req, res);                                      // TODO: log in to user dash
+                    } else {                                                         // if password is wrong case
+                        res.render('name', {csrfToken: req.csrfToken, err: true});   // re-render name page
+                    }
+                } else {                                                             // given no user make one
+                    var user = new mongo.user({                                      // create user document
+                        name: req.body.name,                                         // give rando's name
+                        password: userAct.hash.hashSync(req.body.password, userAct.hash.genSaltSync(10)), // hash rando's password
+                        type: 'freeloader'                                           // what type of account is this?
+                    });
+                    user.save(function(err){
+                        if(err){res.render('name', {csrfToken: req.csrfToken, err: err});} // user failed to save
+                        else{userAct.chat(req, res);}                                      // user saved, log em in
+                    });
+                }
+            });
+        } else if(req.body.name){userAct.chat(req, res);}  // no password but we have a name condition
+        else {res.redirect('/signin');}                    // maybe re-render with an error message
+    },
+    chat: function(req, res){
+        req.session.user = {name: req.body.name};
+        res.redirect('/');
     },
     room: function(req, res){ // check if this is a legit room else redirect to randochat
-        if(req.params.username === 'easteregg'){
-            userAct.auth(req, res);
-        } else {res.redirect('/');}
-    }
+        mongo.user.findOne({name: req.params.username}, function(err, rando){
+            if(rando){
+                userAct.auth(req, res);
+            } else {
+                res.send('not legit!');
+            }
+        });
+    },
+    renderSignin: function(req, res){res.render('name', {csrfToken: req.csrfToken()});},
 }
 
 var cookie = { // depends on client-sessions and mongo
@@ -90,23 +126,23 @@ var serve = {
     parse: require('body-parser'),
     theSite: function (){
         var app = serve.express();
-        var http = require('http').Server(app);            // http server for express framework
-        app.set('view engine', 'jade');                    // template with jade
-        app.use(require('compression')());                 // gzipping for requested pages
-        app.use(serve.parse.json());                       // support JSON-encoded bodies
-        app.use(serve.parse.urlencoded({extended: true})); // support URL-encoded bodies
-        app.use(cookie.meWant());                          // support for cookies
-
+        var http = require('http').Server(app);              // http server for express framework
+        mongo.init();                                        // connect with mongo and set up schema
+        app.set('view engine', 'jade');                      // template with jade
+        app.use(require('compression')());                   // gzipping for requested pages
+        app.use(serve.parse.json());                         // support JSON-encoded bodies
+        app.use(serve.parse.urlencoded({extended: true}));   // support URL-encoded bodies
+        app.use(cookie.meWant());                            // support for cookies
+        app.use(require('csurf')());                         // Cross site request forgery tokens
         app.use(serve.express.static(__dirname + '/views')); // serve page dependancies (sockets, jquery, bootstrap)
-        var router = serve.express.Router();
-        router.get('/', userAct.auth);                                  // main route for getting into a randochat if you have a name
-        router.get('/signin', function(req, res){res.render('name');}); // where one goes to get a name
-        router.post('/signin', userAct.name);                           // how one create their name
-        router.get('/:username', userAct.room);                         // personal rooms for special users
-        app.use(router);
-
-        sock.listen(http);                            // listen for socket connections
-        http.listen(process.env.PORT);                // listen on specified PORT enviornment variable
+        var router = serve.express.Router();                 // create express router object to add routing events to
+        router.get('/', userAct.auth);                       // main route for getting into a randochat if you have a name
+        router.get('/signin', userAct.renderSignin);         // where one goes to get a name
+        router.post('/signin', userAct.login);               // how one create their name
+        router.get('/:username', userAct.room);              // personal rooms for special users
+        app.use(router);                                     // get express to user the routes we set
+        sock.listen(http);                                   // listen for socket connections
+        http.listen(process.env.PORT);                       // listen on specified PORT enviornment variable
     }
 }
 
