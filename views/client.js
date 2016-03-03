602a29e2b42aae541314f405ead37537781bd99e
paulBeaudet <inof8or@gmail.com>
2016-03-03 13:39:13 -0500

    rebuild history generation system to be more simple

--- views/client.js
+++ views/client.js
@@ -5,34 +5,25 @@ var FULL_TIMEOUT = 15; // timeout for long inactivity
 var MAX_MONO = 60;     // maximum time that can be used to monologe
 var PAUSE_TIMEOUT = 3; // inactivity timeout
 
-var hist = {
-    row: 0,                                    // notes history row being typed in
-    refresh: function(){                       // refresh elements
-        for(var i = 0; i<NUM_ENTRIES; i++){    // for all of the entries
-            $('#dialog'+i).html('');           // clear previous dialog
-            $('#person'+i).html('');           // clear previous users
-        }
-        hist.row = 0;
-        $('#wpm').html('');
-    },
-    increment: function(){ // decides with row to edit to and when the dialog needs to scoot up
-        if(hist.row === NUM_ENTRIES - 1){
-            for(var i = 1; i < NUM_ENTRIES; i++){
-                $('#dialog' + ( i - 1 )).html( $('#dialog' + i).html() );
-                $('#person' + ( i - 1 )).html( $('#person' + i).html() );
-            }
-            $('#dialog' + (NUM_ENTRIES - 1)).html('');
-            $('#person' + (NUM_ENTRIES -1)).html('');
-        }
-        else {hist.row++;}
-    },
-    chat: function(rtt){
-        $('#dialog'+ hist.row).html(rtt.text);                  // incomming chat dialog
-        $('#person'+ hist.row).html(rtt.from);                  // set id of incoming message
+var convo = {
+    items: 0,
+    chat: function(rtt){  // update this users most current message
+        $('.txt:last').text(rtt.text);                          // update message
         myTurn.idle = 0;                                        // reset idle counter
         $('#wpm').html(speed.realTime(rtt.text.length)+' WPM'); // show speed after start
     },
-    taken: function(){ return $('#person'+hist.row).html() === send.to; }, // check if our partner started talking
+    next: function(rtt){
+        if(convo.items === NUM_ENTRIES){$('.message:first').remove();} // make room if entries full
+        else{convo.items++;}                                         // count entries
+        speed.realTime();                                            // start speedometer
+        var nameDiv = $('<span class="name"/>').text(rtt.from).addClass("pull-right text-success");
+        var textDiv = $('<span class="txt"/>').text(rtt.text);
+        $('#history').append($('<div class="message"/>').append(textDiv, nameDiv));
+    },
+    rm: function(){
+        $('.message').remove();
+        convo.items = 0;
+    }
 }
 
 var myTurn = {
@@ -41,10 +32,10 @@ var myTurn = {
     idle: 0,
     clock: 0,
     interrupt: function(rtt){
-        myTurn.set(false);
-        send.clear();
-        myTurn.begin();
-        hist.chat(rtt);
+        myTurn.set(false);   // note its no longer my turn
+        send.clear();        // clear message that was intrupted
+        myTurn.start();      // engage turn starting actions: timer
+        convo.next(rtt);     // display what our partner said
     },
     set: function(status){
         myTurn.isIt = status;                                 // set status of whos turn it is
@@ -52,29 +43,25 @@ var myTurn = {
     },
     start: function(){ // first turn
         myTurn.elapsed = 0;
+        myTurn.idle = 0;
         clearTimeout(myTurn.clock);                    // Make sure standing timeout is removed if any
         myTurn.clock = setTimeout(myTurn.check, 1000); // create new timeout
-    },
-    begin: function(){ // turn by turn
-        hist.increment();
-        speed.realTime();
-        myTurn.start();
     },
     check: function(){
         myTurn.elapsed++;                   // increment elapsed time
         myTurn.idle++;                      // increment idle time (will only really increment w/inactivity)
         if(myTurn.isIt){                    // might this client talk?
-            if($('#person'+hist.row).html()===send.to){myTurn.set(false);} // if someone all ready talk then no
+            if($('.name:last').html() === send.to){myTurn.set(false);} // if someone all ready talk then no
         }else{                              // not this users turn
             if(myTurn.elapsed > OPEN_HELM || myTurn.idle > PAUSE_TIMEOUT){myTurn.set(true);} // check for my turn
         }
         if(myTurn.idle > FULL_TIMEOUT || myTurn.elapsed > MAX_MONO){ // disconnect conditions
             sock.et.emit('end');                                     // signal to server your are ready for a new partner
-            hist.refresh();                                          // clear out going conversation
+            convo.rm();                                              // clear out conversation history
             myTurn.set(false);                                       // block typing
             myTurn.idle = 0;                                         // reset idle to zero
             send.clear();                                            // be sure text box is cleared when disconnecting
-            $('#topnav').fadeIn();
+            $('#topnav').fadeIn(1000);
         } else {myTurn.clock = setTimeout(myTurn.check, 1000);}      // set next timeout when still connected
     }
 }
@@ -88,11 +75,12 @@ var send = {
             if(send.empty){
                 send.empty = false;
                 sock.et.emit('interrupt', rtt);
-                myTurn.begin();
+                convo.next(rtt);
+                myTurn.start();
             } else {
                 sock.et.emit('chat', rtt);       // send real time chat data to partner
+                convo.chat(rtt);                 // fill personal history
             }
-            hist.chat(rtt);                      // write chat real time chat data to self
         } else {send.clear();}                   // block input
     },
     clear: function(){
@@ -113,17 +101,16 @@ var speed = { // -- handles gathing speed information
 var sock = {  // -- handle socket.io connection events
     et: io(), // start socket.io listener
     nick: '', // name of this client
-    name: function(nickName){           // allow chat and go when we have a name
-        sock.nick = nickName;           // learn ones own name
-        sock.et.on('chat', hist.chat);  // recieves real time chat information
+    name: function(nickName){                      // allow chat and go when we have a name
+        sock.nick = nickName;                      // learn ones own name
+        sock.et.on('chat', convo.chat);            // recieves real time chat information
         sock.et.on('interrupt', myTurn.interrupt); // recieves new chat partners or interuptions from partner
         sock.et.on('connect_error', function(){window.location.replace('/');}); // reload on connection error
         sock.et.on('start', function(partner){
-            console.log('connecting with ' + partner );
-            send.to = partner;          // recognize who you're talking to
-            myTurn.set(true);           // give the ability to talk
-            myTurn.start();             // signal begining of turn
-            $('#topnav').fadeOut()
+            send.to = partner;                     // recognize who you're talking to
+            myTurn.set(true);                      // give the ability to talk
+            myTurn.start();                        // signal begining of turn
+            $('#topnav').fadeOut(2000)
         });
     }
 }
